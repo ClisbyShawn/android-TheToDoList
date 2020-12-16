dc160d57f176b15680766b7d259d7431aac89511
Shawn Clisby <shawn.clisby@gmail.com>
2020-12-16 10:26:36 -0500

    Basic navigation and view binding setup.

--- app/src/main/java/com/android/shawnclisby/thetodolist/LoginFragment.kt
+++ app/src/main/java/com/android/shawnclisby/thetodolist/LoginFragment.kt
@@ -5,25 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.android.shawnclisby.thetodolist.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        
+        binding.buttonLoginSignIn.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.mainFragment)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            LoginFragment()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
