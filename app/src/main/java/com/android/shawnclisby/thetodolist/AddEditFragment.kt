c12c7901a31cb84b4fa1d49a4446d0a7defeef86
Shawn Clisby <shawn.clisby@gmail.com>
2021-01-10 16:23:48 -0500

    added new Fragment to Project that Adds or Edits Task depending on item clicked in recyvlerview or adding new task.

--- app/src/main/java/com/android/shawnclisby/thetodolist/AddEditFragment.kt
+++ app/src/main/java/com/android/shawnclisby/thetodolist/AddEditFragment.kt
@@ -1,59 +1,46 @@
 package com.android.shawnclisby.thetodolist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.android.shawnclisby.thetodolist.data.TaskViewModel
+import com.android.shawnclisby.thetodolist.data.TaskViewModelFactory
+import com.android.shawnclisby.thetodolist.databinding.FragmentAddEditBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private var _binding: FragmentAddEditBinding? = null
+    private val binding get() = _binding!!
+
+    private val taskViewModel: TaskViewModel by activityViewModels {
+        TaskViewModelFactory(
+            requireActivity().application
+        )
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit, container, false)
-    }
+    ): View {
+        _binding = FragmentAddEditBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.apply {
+            taskViewModel.taskData?.observe(viewLifecycleOwner) { task->
+                task?.let { editTask->
+                    tieAddEditTitle.setText(editTask.title)
+                    switchAddEditPriority.isChecked = editTask.priority
                 }
             }
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
